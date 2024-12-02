2ee665f170d58a360c1b2740efa5fb1815c9aae6
Margarida Almeida <margarida.almeida.92@gmail.com>
2024-12-02 20:45:31 +0000

    Setting up

---
+++ src/views/MainContent.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Header from '@/components/Header.vue'
+</script>
+
+<template>
+  <Header />
+  <h1>Portfolio</h1>
+  <!-- sections -->
+  <!-- main
+   about me
+   experience
+   projects
+   contacts -->
+</template>
+
+<style lang="scss"></style>
